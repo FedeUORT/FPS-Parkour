365fa93680f251350f11f6735c99ec2712280fa8
FedeUORT <isagym18@gmail.com>
2025-05-19 08:57:45 -0300

    Parkour avances. Respawn.

---
+++ Assets/Scripts/RespawnOnFall.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RespawnOnFall : MonoBehaviour
+{
+    [SerializeField] float yForRespawn;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (transform.position.y<= yForRespawn)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+
+    }
+}
