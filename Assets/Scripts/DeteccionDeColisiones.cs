c45e9247b3dfe70235c49cae7c84e6ba57da4fcd
FedeUORT <isagym18@gmail.com>
2025-05-23 10:38:28 -0300

    avances

---
+++ Assets/Scripts/DeteccionDeColisiones.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeteccionDeColisiones : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision col)
+    {
+        Debug.Log("Contacto");
+        Destroy(col.gameObject);
+    }
+}
